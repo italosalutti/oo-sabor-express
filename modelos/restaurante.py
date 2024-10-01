cbc648863e2d80dfd2810085f092037f5368f6c1
Italo Salutti <salutti@gmail.com>
2024-10-01 10:51:13 -0300

    correção do código e criação do app

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,12 +1,12 @@
 class Restaurante:
     restaurantes = []
-    
+
     def __init__(self, nome, categoria):
-        self._nome = nome.title() # ".title" para a primeira letra de cada palavra ser maiúscula
-        self._categoria = categoria.upper() # ".upper" para todas as letras ficarem em caixa alta
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
-        
+    
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
@@ -14,16 +14,17 @@ class Restaurante:
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
-            
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
     @property
     def ativo(self):
-        return '✅' if self._ativo else '❌'
+        return '⌧' if self._ativo else '☐'
     
     def alternar_estado(self):
         self._ativo = not self._ativo
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
